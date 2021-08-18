745e27f99c2ddc0f4c2b9071d1c9b0d2de14ed4d
ThangDN6 <sgooophx@gmail.com>
2021-08-18 14:16:48 +0700

    Apply

--- app/src/main/java/com/example/mvvmprojecttemplate/activities/MainActivity.kt
+++ app/src/main/java/com/example/mvvmprojecttemplate/activities/MainActivity.kt
@@ -2,18 +2,11 @@ package com.example.mvvmprojecttemplate.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.Animation
-import android.view.animation.BounceInterpolator
-import android.view.animation.CycleInterpolator
-import android.view.animation.TranslateAnimation
-import android.widget.Button
-import android.widget.ImageView
 import com.example.mvvmprojecttemplate.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
